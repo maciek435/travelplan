6ae6045c699b318dcfad1ca6740318708dff60b3
Maciek <admin@admin.com>
2026-05-19 14:03:39 +0200

    feat: add database connection setup

---
+++ app/db/base.py
@@ -0,0 +1,11 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
