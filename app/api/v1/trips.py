aab1fc9a340bc9e87dd1f90ef60da349ea683230
maciek <maciek@host.com>
2026-05-30 19:01:00 +0200

    feat: add full trips CRUD

--- app/api/v1/trips.py
+++ app/api/v1/trips.py
@@ -30,5 +30,39 @@ async def get_users_trips(current_user: User = Depends(get_current_user), db: As
     trips = result.scalars().all()
     return trips
 
+@router.get("/{id}", response_model=TripResponse)
+async def get_trip(id: int, current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Trip).where(Trip.owner_id == current_user.id, Trip.id == id))
+    trip = result.scalar_one_or_none()
+    if trip is None:
+        raise HTTPException(status_code=404, detail="Nie znaleziono podróży")
+    return trip
 
-   
+@router.put("/{id}", response_model=TripResponse)
+async def update_trip(id: int, trip_data: TripCreate, current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Trip).where(Trip.owner_id == current_user.id, Trip.id == id))
+    trip = result.scalar_one_or_none()
+    if trip is None:
+        raise HTTPException(status_code=404, detail="Nie znaleziono podróży")
+    
+    trip.title = trip_data.title
+    trip.destination = trip_data.destination
+    trip.start_date = trip_data.start_date
+    trip.end_date = trip_data.end_date
+
+    await db.commit()
+    await db.refresh(trip)
+    
+    return trip
+
+@router.delete("/{id}")
+async def delete_trip(id: int, current_user: User = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Trip).where(Trip.owner_id == current_user.id, Trip.id == id))
+    trip = result.scalar_one_or_none()
+    if trip is None:
+        raise HTTPException(status_code=404, detail="Nie znaleziono podróży")
+    
+    await db.delete(trip)
+    await db.commit()
+
+    return {"messsage": "podróż usunięta"}
