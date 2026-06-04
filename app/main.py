0cd3fcae4838bd43f3cace471992835adf9bd90e
maciek <maciek@host.com>
2026-06-04 21:47:37 +0200

    feat: add CORS middleware

--- app/main.py
+++ app/main.py
@@ -6,8 +6,19 @@ from fastapi import FastAPI
 from app.api.v1.auth import router as auth_router
 from app.api.v1.trips import router as trips_router
 from app.api.v1.day_tasks import router as day_tasks
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173",
+                   "http://127.0.0.1:5173",
+                   ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router, prefix='/auth', tags=["auth"])
 app.include_router(trips_router, prefix='/trips', tags=["trips"])
 app.include_router(day_tasks, prefix='/day-tasks', tags=["day-tasks"])
